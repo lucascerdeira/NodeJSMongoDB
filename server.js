f00e6775d334c42ea36753164d898cb164b98a0f
Lucas Almeida Cerdeira <devlucascerdeira@gmail.com>
2023-01-04 15:33:25 -0300

    crud finalized

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
+//puxando as rotas do app
 import app from './src/app.js';
 
 // vamos condicionar a porta
 //ou ela ficara em um hambiente de produção ou ficara fixa
 const port = process.env.PORT || 3000;
-
-
 
 // research arrow function
 app.listen(port, () => {
